09d9a97e986f0e7ed54e321b2fb091e02937cca4
Coleman Buffa <coleman.buffa@gmail.com>
2021-03-01 21:32:09 -0800

    Prepare server for connecting to Heroku.

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Start the API server
-db.sequelize.sync().then(function () {
+db.sequelize.sync({force:true}).then(function () {
   app.listen(PORT, function () {
     console.log(
       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
